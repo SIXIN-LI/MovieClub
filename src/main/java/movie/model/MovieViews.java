381ad7390e90c984825a09023ec983a7412cd6b6
xiaoxiaoliu2022 <98120990+xiaoxiaoliu2022@users.noreply.github.com>
2023-03-11 20:20:28 -0800

    delete super, update variable names

--- src/main/java/movie/model/MovieViews.java
+++ src/main/java/movie/model/MovieViews.java
@@ -5,61 +5,85 @@ import java.util.Date;
 public class MovieViews{
 	
 	protected int  viewId;
-	protected Users userId;
-	protected Movies movieId;
+	protected Users user;
+	protected Movies movie;
 	protected Date viewTime;
 	
-	public MovieViews(int viewId, Users userId, Movies movieId, Date viewTime) {
+	
+
+	public MovieViews(int viewId, Users user, Movies movie, Date viewTime) {
 
 		this.viewId = viewId;
-		this.userId = userId;
-		this.movieId = movieId;
+		this.user = user;
+		this.movie = movie;
 		this.viewTime = viewTime;
 	}
+
+
 
 	public MovieViews(int viewId) {
 
 		this.viewId = viewId;
 	}
 
-	public MovieViews(Users userId, Movies movieId, Date viewTime) {
-		super();
-		this.userId = userId;
-		this.movieId = movieId;
+
+
+	public MovieViews(Users user, Movies movie, Date viewTime) {
+
+		this.user = user;
+		this.movie = movie;
 		this.viewTime = viewTime;
 	}
+
+
 
 	public int getViewId() {
 		return viewId;
 	}
 
+
+
 	public void setViewId(int viewId) {
 		this.viewId = viewId;
 	}
 
-	public Users getUserId() {
-		return userId;
+
+
+	public Users getUser() {
+		return user;
 	}
 
-	public void setUserId(Users userId) {
-		this.userId = userId;
+
+
+	public void setUser(Users user) {
+		this.user = user;
 	}
 
-	public Movies getMovieId() {
-		return movieId;
+
+
+	public Movies getMovie() {
+		return movie;
 	}
 
-	public void setMovieId(Movies movieId) {
-		this.movieId = movieId;
+
+
+	public void setMovie(Movies movie) {
+		this.movie = movie;
 	}
+
+
 
 	public Date getViewTime() {
 		return viewTime;
 	}
 
+
+
 	public void setViewTime(Date viewTime) {
 		this.viewTime = viewTime;
 	}
+
+	
 	
 	
 	
